cb3c9f15b5bca24feba8a7c4136d8101b828a8b3
pmaharjan124 <maharjanpuja@gmail.com>
2020-04-16 17:20:01 -0400

    first commit

---
+++ src/IdentifyingVariables.java
@@ -0,0 +1,22 @@
+public class IdentifyingVariables {
+    public static void main(String[] args)
+    {
+        String greeting = ("Hey Mom and Dad!"); // first message
+         String bodymsg = ("Here are some interesting details about the first country I'm visiting."); //Body of the message
+        String nameOfCountry = ("Kenya");
+    double population = 49.7d;
+    String capital = ("Nairobi");
+    String currency = ("One U.S Dollar equals $103.84 Kenyan Shillings!!");
+    String lastMsg = ("I,m so excited to visit the Nairobi Nation Park and feel the giraffes!! Talk later.");
+
+    System.out.println(greeting);
+        System.out.println(bodymsg);
+        System.out.println("Name: " +nameOfCountry);
+        System.out.println("Population Size: " +population + "million");
+        System.out.println("Capital: " +capital);
+        System.out.println("Currency: "+currency);
+        System.out.println(lastMsg);
+
+
+    }
+}
